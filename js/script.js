7f3427403e9dcc789a5b903f6158e61dc491b3f6
MyNameIsAlpaca <Saragosagabriele@gmail.com>
2023-02-23 14:29:40 +0100

    feat: html base, js start

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/* 
+1 Chiedere il nome del passeggero
+2 chiedere km da percorrere
+3 chiedere l'età
+4 calcolare il prezzo del biglietto in base alla distanza da percorrere considerando
+il prezzo di 0.21€/km.
+   * Se l'età del passeggero è minore di 18 anni applicare uno sconto del 20%
+   * Se l'età è maggiore di 65 anni applicare uno sconto del 40%
+5 generare biglietto con i dati del passeggero e il prezzo
+*/
